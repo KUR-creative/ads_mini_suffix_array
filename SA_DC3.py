148efdedad3f1e80bab4a5589cf2811f877af778
kur <rhdnfka94@gmail.com>
2020-06-11 17:33:44 +0900

    Use dict instead of list for sa<->rank mapping

--- SA_DC3.py
+++ SA_DC3.py
@@ -1,3 +1,5 @@
+from bidict import bidict
+import funcy as F
 import itertools as I
 
 tup = lambda f: lambda argtup: f(*argtup)
@@ -16,6 +18,7 @@ def window(seq, n=2):
         result = result[1:] + (elem,)
         #yield ''.join(result)
         yield result
+        
 #------------------- naive implementations ---------------------
 def suffix_seq(s):
     for i in range(len(s)):
@@ -35,91 +38,98 @@ def pad3x(ints, padval=0):
     num_pad = (3 - len(ints) % 3) % 3
     return ints + [padval] * num_pad
 
-def origin_indexes(string):
-    l = len(string)
+def origin_indexes(length):
     return (
-        list(range(0,l,3)),
-        list(range(1,l,3)),
-        list(range(2,l,3))
+        list(range(0,length,3)),
+        list(range(1,length,3)),
+        list(range(2,length,3))
     )
 
 def suffix_array(imap, string):
-    unsorted_s0idx, s1idx, s2idx = origin_indexes(string)
-    #print(unsorted_s0idx, s1idx, s2idx)
-
+    #[1] Get index lists, reduced string
+    unsorted_s0idx, s1idx, s2idx = origin_indexes(len(string))
     rs = pad3x([imap[c] for c in string]) + [0,0,0] #reduced string
+    #print(unsorted_s0idx, s1idx, s2idx)
     #print('   rs:',rs)
-
-    # index of origin string. part of SA
-    # index of s1s2idx is grade(nth). (sorted)
-    s1s2idx = sorted( 
+    
+    #[2] Get (flawed) s1+s2 suffix array.
+    #It can't be SA if some elems are duplicated.
+    #this can't allow duplicated elem.. 
+    s12_sa = sorted( 
         s1idx + s2idx, 
         key=lambda i: rs[i:i+3])
     # we can know s1 or s2 by calculating modular of index.
-    #print([rs[i:i+3] for i in s1idx + s2idx])
-    print('12idx:', s1s2idx)
+    print('s12_sa:', s12_sa)
 
-
-    # inverse mapping of s1s2idx. part of inverse SA
-    s1s2nth = dict((s,i) for i,s in enumerate(s1s2idx))
-    #print('12nth:', s1s2nth)
-    
-    # indexes of origin string, sorted by s0. part of SA. 
-    s0idx = sorted(
-        unsorted_s0idx, key=lambda i: [rs[i], s1s2nth.get(i+1, 0)])
-
-    #print(' 0idx:', s0idx)
-
-    #(if s1s2idx values are distinct,) merge with s0
-    len_s1s2 = len(s1s2idx)
-    len_s0 = len(s0idx)
-    len_sa = len_s1s2 + len_s0
-    suff_arr = []
-    i12 = i0 = 0
-    
-    #print('----')
-    while i12 + i0 < len_sa:
-        beg = s1s2idx[i12]
-        Sno = beg % 3
-        beg0 = s0idx[i0]
-        
-        # make form for comparison
-        form12 = [*rs[beg:beg+Sno], s1s2nth.get(beg+Sno, 0)]
-        form0 = [*rs[beg0:beg0+Sno], s1s2nth.get(beg0+Sno, 0)]
-        assert len(form12) == len(form0), \
-            f'form12 = {form12} != {form0} = form0'
-        
-        #print(form12, form0)
-        if form12 < form0:
-            suff_arr.append(beg)
-            i12 += 1
-        elif form12 > form0:
-            suff_arr.append(beg0)
-            i0 += 1
+    #[3] Check uniqueness of elems and get rank ordered by s12_sa
+    all_unique = True
+    s1s2_rank = [0]
+    rank = 0
+    for idx0,idx1 in window(s12_sa):
+        # Check 3chars of s12_sa duplication pairwisely.
+        if rs[idx0:idx0+3] == rs[idx1:idx1+3]:
+            all_unique = False
         else:
-            assert False, 'do not reach'
-        #print(Sno, '|', i12, i0, ':', len_s1s2, len_s0, '|', beg, beg0, '|', form12, form0)
-        #print(suff_arr)
+            rank += 1
+        s1s2_rank.append(rank)
+    print('all_unique:', all_unique)
+    print(s1s2_rank)
+    
+    #[4] Get rank and SA (s1-s2 ordered) if all elems are unique
+    s12_sa2rank = dict(zip(s12_sa, s1s2_rank))
+    if all_unique:
+        s12_rank2sa = dict(zip(s1s2_rank, s12_sa))
+        #print(s12_sa2rank)
+        #print(s12_rank2sa)
+        s0_sa = sorted(
+            unsorted_s0idx,
+            key=lambda i: [rs[i], s12_sa2rank.get(i+1, 0)])
+        #print(' 0idx:', s0_sa)
 
-        if i0 == len_s0:
-            suff_arr = suff_arr + s1s2idx[i12:]
-            break
-        if i12 == len_s1s2:
-            suff_arr = suff_arr + s0idx[i0:]
-            break
+        #[5] if unique, merge with s0!
+        len_s1s2 = len(s12_sa)
+        len_s0 = len(s0_sa)
+        len_sa = len_s1s2 + len_s0
+        suff_arr = []
+        i12 = i0 = 0
+        #print('----')
+        while i12 + i0 < len_sa:
+            beg = s12_sa[i12]
+            Sno = beg % 3
+            beg0 = s0_sa[i0]
+            # make form for comparison
+            form12 = [*rs[beg:beg+Sno], s12_sa2rank.get(beg+Sno, 0)]
+            form0 = [*rs[beg0:beg0+Sno], s12_sa2rank.get(beg0+Sno, 0)]
+            assert len(form12) == len(form0), \
+                f'form12 = {form12} != {form0} = form0'
+            #print(form12, form0)
+            if form12 < form0:
+                suff_arr.append(beg)
+                i12 += 1
+            elif form12 > form0:
+                suff_arr.append(beg0)
+                i0 += 1
+            else:
+                assert False, 'do not reach'
+            #print(Sno, '|', i12, i0, ':', len_s1s2, len_s0, '|', beg, beg0, '|', form12, form0)
+            #print(suff_arr)
+            if i0 == len_s0:
+                suff_arr = suff_arr + s12_sa[i12:]
+                break
+            if i12 == len_s1s2:
+                suff_arr = suff_arr + s0_sa[i0:]
+                break
 
-    #print('ans:', suff_arr)
-    return suff_arr
-
-    #s0idx = # ordered by s1
-    #[5, 1, 4, 2]
-    #s1s2nth = [
+        return suff_arr
+    #else:
+    # if not, make s12_sa, s12_sa2rank by recursing suffix_array
     
 imap = int_map('ab$')
-#string = 'aa$'
-#string = 'aaa$'
-#string = 'aababa$'
-imap = int_map('acgt$'); string = 'acccc$'
+string = 'aa$'
+string = 'aaa$'
+#imap = int_map('ac$'); string = 'aac$'
+string = 'aababa$'
+#imap = int_map('acgt$'); string = 'acccc$'
 
-print(suffix_array(imap, string))
-print([x for x,_ in naive_suffix_map(string)])
+print('actual:', suffix_array(imap, string))
+print('expect:', [x for x,_ in naive_suffix_map(string)])
